2a6617b80874a519e7232fdd0a9541d5288ff04b
Gary Phillips <garythellama@gmail.com>
2020-06-20 00:07:53 -0700

    Add styling to Card component
    It looks terrible, but that's a problem for another day!

--- client/src/Card.js
+++ client/src/Card.js
@@ -1,9 +1,16 @@
-import * as React from 'react'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
 import { PropTypes } from 'prop-types'
 
 export const Card = ({ text, profileImageUrl, name, username }) => {
     return (
-        <div className="card">
+        <div css={css`
+            box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+            border-style: solid;
+            border-radius: 10px;
+            margin: 20px 20px 20px 20px;
+            padding: 20px 0 20px 0;
+        `}>
             <User {...{profileImageUrl, name, username}} />
             {text}
         </div>
